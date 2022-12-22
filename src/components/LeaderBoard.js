b0759c62cd894775bcbe369e464abdb291c07f60
zwehr <zwehrmeister@gmail.com>
2022-12-21 22:56:35 -0500

    sort leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -4,12 +4,14 @@ function LeaderBoard(props) {
   return (
     <div className='LeaderBoard'>
       <h2>Leaderboard</h2>
-      {props.leaderboardScores.map((scoreLine) =>
-        <LeaderBoardLine
-          key={scoreLine.uuid}
-          name={scoreLine.name}
-          score={scoreLine.score}
-        />)}
+      {props.leaderboardScores
+        .sort(({ score: previousScore }, { score: currentScore }) => currentScore - previousScore)
+        .map((scoreLine) =>
+          <LeaderBoardLine
+            key={scoreLine.uuid}
+            score={scoreLine.score}
+            name={scoreLine.name}
+          />)}
     </div>
   )
 }
